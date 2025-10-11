da22325f43fd703e607b6a2ae0d8927655945b75
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-11 22:49:05 +0530

    initial commit to understand the @DependsOn

---
+++ 12-DependsOn-Annotation-App/src/main/java/com/alchemist/UserDao.java
@@ -0,0 +1,24 @@
+package com.alchemist;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDao implements InitializingBean,DisposableBean{
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Getting data from db..");
+		System.out.println("Storing into the redis..");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Closing the connections..");
+		
+	}
+
+}
