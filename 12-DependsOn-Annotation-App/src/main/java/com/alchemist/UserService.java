eca2b9ceb5d02e8c9951f70f147ae138eabb32d8
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-11 22:55:54 +0530

    Added @DependsOn

--- 12-DependsOn-Annotation-App/src/main/java/com/alchemist/UserService.java
+++ 12-DependsOn-Annotation-App/src/main/java/com/alchemist/UserService.java
@@ -1,8 +1,10 @@
 package com.alchemist;
 
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
 @Service
+@DependsOn("userDao")
 public class UserService {
 	public UserService() {
 		System.out.println("Getting data from Redis...");
