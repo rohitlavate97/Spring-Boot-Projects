9403e6dc4733bdc1cfd7d2dc88face5fb192080c
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-12 21:56:45 +0530

    Added first spring boot project

---
+++ 13-SB-first-App/src/main/java/com/alchemist/Application.java
@@ -0,0 +1,22 @@
+package com.alchemist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome";
+		
+	}
+
+}
