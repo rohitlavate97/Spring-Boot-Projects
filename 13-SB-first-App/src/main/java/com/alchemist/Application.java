f677acbbae96c57d553ba239ade92882b8807b56
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-13 17:49:09 +0530

    Added banner

--- 13-SB-first-App/src/main/java/com/alchemist/Application.java
+++ 13-SB-first-App/src/main/java/com/alchemist/Application.java
@@ -2,15 +2,18 @@ package com.alchemist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(Application.class, args);
+		System.out.println(context.getClass().getName());
 	}
 	
 	@GetMapping("/welcome")
