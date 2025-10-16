cb43f83bc1c777eaa8737c0f03bf667254d88546
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-16 16:36:06 +0530

    Added JPA properties so that we can save records into db and fetch it

--- 21-Web-MVC-StudentApp/src/main/java/com/alchemist/service/StudentService.java
+++ 21-Web-MVC-StudentApp/src/main/java/com/alchemist/service/StudentService.java
@@ -3,16 +3,33 @@ package com.alchemist.service;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alchemist.binding.Student;
+import com.alchemist.entity.StudentEntity;
+import com.alchemist.repository.StudentRepository;
 
 @Service
 public class StudentService {
+	
+	@Autowired
+	private StudentRepository repo;
+	
 	public List<String> getCourses(){
 		return Arrays.asList("JAVA","PYTHON","DEVOPS","AWS");
 	}
 	
 	public List<String> getTimings(){
 		return Arrays.asList("Morning","Afternoon","Evening");
+	}
+	
+	public boolean saveStudent(Student student) {
+		StudentEntity entity = new StudentEntity();
+		BeanUtils.copyProperties(student, entity);
+		repo.save(entity);
+		return false;
 	}
 
 }
