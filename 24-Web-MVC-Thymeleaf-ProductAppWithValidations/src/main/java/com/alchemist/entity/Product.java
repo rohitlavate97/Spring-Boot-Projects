242cb965c6fd9da61ec54c3dd23e7288751fc0f5
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-17 21:38:21 +0530

    Thymeleaf project for Product, sql server database--->working example

---
+++ 24-Web-MVC-Thymeleaf-ProductAppWithValidations/src/main/java/com/alchemist/entity/Product.java
@@ -0,0 +1,23 @@
+package com.alchemist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer pid;
+	
+	private String name;
+	
+	private Double price;
+	
+	private Integer quantity;
+
+}
