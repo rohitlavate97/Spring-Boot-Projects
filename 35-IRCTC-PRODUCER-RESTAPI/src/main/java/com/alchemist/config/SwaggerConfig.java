926899857ede7ec586a7d47fbac681c6b0024f8f
rohitlavate97 <rohitlavate97@gmail.com>
2025-10-20 22:17:19 +0530

    Added Swagger documentation for REST APIs

---
+++ 35-IRCTC-PRODUCER-RESTAPI/src/main/java/com/alchemist/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.alchemist.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/*@EnableSwagger2*/
+@Configuration
+public class SwaggerConfig {
+	/*
+	 * @Bean //To customizd docket object @Bean used public Docket apiDoc() { return
+	 * new Docket(DocumentationType.SWAGGER_2).select()
+	 * .apis(RequestHandlerSelectors.basePackage("com.alchemist.rest"))
+	 * .paths(PathSelectors.any()) //in class generate documentation for all
+	 * methods, we can mention post or get .build(); }
+	 */
+
+}
