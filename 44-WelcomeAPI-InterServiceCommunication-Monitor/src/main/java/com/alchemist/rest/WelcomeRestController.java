fec22441f5f25150c55fa7ddc57d7b82f4de16b1
Rohit <rohit-mac@Rohits-MacBook-Pro.local>
2025-10-24 17:27:43 +0530

    InterService communication

---
+++ 44-WelcomeAPI-InterServiceCommunication-Monitor/src/main/java/com/alchemist/rest/WelcomeRestController.java
@@ -0,0 +1,14 @@
+package com.alchemist.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return "Welcome to API";
+	}
+
+}
