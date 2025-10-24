aaaf17809049719d39162a8dd59520d216f10d70
Rohit <rohit-mac@Rohits-MacBook-Pro.local>
2025-10-24 18:25:22 +0530

    RestTemplate added

--- 44-WelcomeAPI-InterServiceCommunication-Monitor/src/main/java/com/alchemist/rest/WelcomeRestController.java
+++ 44-WelcomeAPI-InterServiceCommunication-Monitor/src/main/java/com/alchemist/rest/WelcomeRestController.java
@@ -1,14 +1,19 @@
 package com.alchemist.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class WelcomeRestController {
 	
 	@GetMapping("/welcome")
 	public String welcomeMsg() {
-		return "Welcome to API";
+		RestTemplate rt = new RestTemplate();
+		ResponseEntity<String> forEntity = rt.getForEntity("http://localhost:9090/greet", String.class);
+		String greetResponse = forEntity.getBody();
+		return greetResponse + " Welcome to REST";
 	}
 
 }
