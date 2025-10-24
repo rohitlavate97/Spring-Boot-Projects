f8c9e8fcb675f013b63f37ba901d9fb6e8c3fdf8
Rohit Lavate <rohitlavate97@gmail.com>
2025-10-24 21:11:11 +0530

    Done changes in Greetcontroller for load balancing
    -Changed port in application.yml of WelcomeAPI project

--- 43-GREET-API/src/main/java/com/alchemist/rest/GreetController.java
+++ 43-GREET-API/src/main/java/com/alchemist/rest/GreetController.java
@@ -1,14 +1,20 @@
 package com.alchemist.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/greet")
 	public String getGreetMsg() {
-		return "Good Morning";
+		String port = env.getProperty("server.port");
+		return "Good Morning("+port+")";
 	}
 
 }
