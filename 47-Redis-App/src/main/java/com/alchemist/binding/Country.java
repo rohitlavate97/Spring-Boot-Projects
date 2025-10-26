cee6b78e2ae2ed04587a9b0015b6a3b9f1af28e3
Rohit Lavate <rohitlavate97@gmail.com>
2025-10-26 18:01:39 +0530

    Implementing Redis

---
+++ 47-Redis-App/src/main/java/com/alchemist/binding/Country.java
@@ -0,0 +1,16 @@
+package com.alchemist.binding;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class Country implements Serializable{
+	
+	private Integer sno;
+	
+	private String name;
+	
+	private String countryCode;
+
+}
---
+++ 47-Redis-App/src/main/java/com/alchemist/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.alchemist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.alchemist.binding.Country;
+
+@Configuration
+public class RedisConfig {
+	@Bean
+	public JedisConnectionFactory jedisconn() {
+		JedisConnectionFactory jedis = new JedisConnectionFactory();
+		//we can set redis server properties here
+		/*
+		 * jedis.setHostName("127.0.0.1"); jedis.setPort(6379);
+		 * jedis.setPassword("yourPassword");
+		 */
+		return jedis;
+	}
+	
+	@Bean
+	public RedisTemplate<Integer, Country> redisTemplate(){         //kind of data stored in redis i.e Country type
+		RedisTemplate<Integer, Country> rt = new RedisTemplate<>();    
+		rt.setConnectionFactory(jedisconn());             //Injecting JedisConnectionFactory object here
+		return rt;
+	}
+	
+	
+}
