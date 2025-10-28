479bf8d12feb4b9f426ff774e30a6f7ea9b05e52
Rohit Lavate <rohitlavate97@gmail.com>
2025-10-28 19:36:26 +0530

    Added CircuitBreaker project

---
+++ 52-Circuit_breaker-LoanService/src/main/java/com/alchemist/controller/LoanController.java
@@ -0,0 +1,21 @@
+package com.alchemist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alchemist.dtos.InterestRate;
+import com.alchemist.services.LoanService;
+
+@RestController
+public class LoanController {
+	@Autowired
+	private LoanService loanService;
+	
+	@GetMapping(path ="/loans")
+	public ResponseEntity<InterestRate> getLoansByType(@RequestParam("type") String type){
+		return ResponseEntity.ok().body(loanService.getAllLoansByType(type.toUpperCase()));
+	}
+}
---
+++ 52-Circuit_breaker-LoanService/src/main/java/com/alchemist/services/LoanService.java
@@ -0,0 +1,31 @@
+package com.alchemist.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.alchemist.dtos.InterestRate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@Service
+public class LoanService {
+	@Autowired
+	private RestTemplate restTemplate;
+	private static final String SERVICE_NAME = "loan-service";
+	private static final String RATE_SERVICE_URL ="";
+	
+	@CircuitBreaker(name =SERVICE_NAME, fallbackMethod = "getDefaultLoan")
+	public InterestRate getAllLoansByType(String type) {
+		System.out.println("*****Original Method called...*****");
+		ResponseEntity<InterestRate> response = restTemplate.getForEntity(RATE_SERVICE_URL, InterestRate.class, type);
+		return response.getBody();
+	}
+	
+	public InterestRate getDefaultLoan(Exception e) {
+		System.out.println("*****Fallback method called.....****");
+		return new InterestRate();      //returning dummy response instead of error response to the client
+	}
+
+}
