a971c639307072ff2aea33ceaa34e33e7b7805ac
Rohit Lavate <rohitlavate97@gmail.com>
2025-10-28 13:39:50 +0530

    modified kafka-consumer app

--- 51-Apache-Kafka-Consumer/src/main/java/com/alchemist/controller/ConsumerRestController.java
+++ 51-Apache-Kafka-Consumer/src/main/java/com/alchemist/controller/ConsumerRestController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/consumer")
 public class ConsumerRestController {
 
     @Autowired
